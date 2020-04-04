d411d45d33a130c084f5901f5fa8e8c1a807f0b1
Soe-HtetNaung <52348060+Soe-HtetNaung@users.noreply.github.com>
2020-04-04 11:37:59 +0800

    refactored methods & added random score and its result

--- Prac03/Broken_Score.py
+++ Prac03/Broken_Score.py
@@ -2,17 +2,28 @@
 CP1404/CP5632 - Practical
 Broken program to determine score status
 """
+import random
 
 def main():
     score = float(input("Enter score: "))
+    result = check_score(score)
+    print(result)
+    randscore = random.randint(0, 100)
+    randresult = check_score(randscore)
+    print("Random score : ", randscore)
+    print(randresult)
+
+def check_score(score):
     if score < 0 or score > 100:
-        print("Invalid score")
+        output = "Invalid score"
     elif score >= 90:
-        print("Excellent")
+        output = "Excellent"
     elif score >= 50:
-        print("Passable")
+        output = "Passable"
     elif score < 50:
-        print("Bad")
+        output = "Bad"
+    return output
+
 
 if __name__ == '__main__':
     main()
