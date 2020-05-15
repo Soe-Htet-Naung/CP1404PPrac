fd75087eeecf2935dfc4e2f752e9a84c05376afe
Soe-HtetNaung <52348060+Soe-HtetNaung@users.noreply.github.com>
2020-05-15 17:53:59 +0800

    Commit account Test

--- Prac01/Broken_Score.py
+++ Prac01/Broken_Score.py
@@ -3,6 +3,7 @@ CP1404/CP5632 - Practical
 Broken program to determine score status
 """
 
+
 def main():
     score = float(input("Enter score: "))
     if score < 0 or score > 100:
@@ -13,6 +14,7 @@ def main():
         print("Passable")
     elif score < 50:
         print("Bad")
+
 
 if __name__ == '__main__':
     main()
